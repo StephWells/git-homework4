dcfc231113d78edcc32b49e84a258dd16bae5bef
StephWells <sw.stephwells@gmail.com>
2020-10-15 22:03:51 -0700

    Task1-Part2

--- task1.py
+++ task1.py
@@ -16,5 +16,42 @@ def main():
         print(usersNumber, 'IS a prime number')
     else:
         print(usersNumber, 'IS NOT a prime number')
-
+    print()
+    print('Now I will randomly generate 6 numbers and I will determine if they are prime numbers...')
+    print()
 main()
+
+def random():
+    import random
+    random1 = random.randint(1,100)
+    random2 = random.randint(1,100)
+    random3 = random.randint(1,100)
+    random4 = random.randint(1,100)
+    random5 = random.randint(1,100)
+    random6 = random.randint(1,100)
+
+    if is_prime(random1):
+        print('The random number', random1, 'is a prime number.')
+    else:
+        print('The random number', random1, 'is not a prime number.')
+    if is_prime(random2):
+        print('The random number', random2, 'is a prime number.')
+    else:
+        print('The random number', random2, 'is not a prime number.')
+    if is_prime(random3):
+        print('The random number', random3, 'is a prime number.')
+    else:
+        print('The random number', random3, 'is not a prime number.')
+    if is_prime(random4):
+        print('The random number', random4, 'is a prime number.')
+    else:
+        print('The random number', random4, 'is not a prime number.')
+    if is_prime(random5):
+        print('The random number', random5, 'is a prime number.')
+    else:
+        print('The random number', random5, 'is not a prime number.')
+    if is_prime(random6):
+        print('The random number', random6, 'is a prime number.')
+    else:
+        print('The random number', random6, 'is not a prime number.')
+random()
